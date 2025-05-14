1ef3d3673bdb9716495f5b9551fce01a365b89c3
TradZz <155823031+TradZz@users.noreply.github.com>
2025-05-14 11:54:14 +0700

    Create password.js

---
+++ password.js
@@ -0,0 +1,17 @@
+{
+
+  "secretCode": "TradzSigma",
+
+  "description": "Jinshi New Meta", 
+
+  "lastUpdated": "14/05/2025",
+
+  "author": "TradzID", 
+
+  "status": "Active",
+
+  "license": "MIT",
+
+  "version": "6.0"
+
+    }
